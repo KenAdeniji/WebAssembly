6cec159e53b08c75773851eb0485ad2a995b8eb6
Ken Adeniji <kenadeniji@hotmail.com>
2022-06-02 07:47:07 -0700

    2022-06-02T0742GitHub.bat

--- Fibonacci/FibonacciNumberBuild.bat
+++ Fibonacci/FibonacciNumberBuild.bat
@@ -1,6 +1,7 @@
 @echo off
+emcc -v
 REM append || exit 1
 REM emcc FibonacciNumber.c FibonacciHelper.c -o FibonacciNumber.js -O3 -s WASM=1 --shell-file ../html_template/shell_minimal.html -s NO_EXIT_RUNTIME=1  -s "EXPORTED_RUNTIME_METHODS=['ccall']" || exit 1
-emcc FibonacciNumber.c FibonacciHelper.c -o FibonacciNumber.js -O3 -s WASM=1 --shell-file ../html_template/shell_minimal.html -s NO_EXIT_RUNTIME=1  -s "EXPORTED_RUNTIME_METHODS=['ccall']"
+emcc FibonacciNumber.c FibonacciHelper.c -o FibonacciNumber.js -O3 -s WASM=1 --shell-file ../html_template/shell_minimal.html -s NO_EXIT_RUNTIME=1  -s "EXPORTED_RUNTIME_METHODS=['ccall']" -v
 REM emrun --port 8080 .
 REM emrun --port 8080 FibonacciNumber.html
